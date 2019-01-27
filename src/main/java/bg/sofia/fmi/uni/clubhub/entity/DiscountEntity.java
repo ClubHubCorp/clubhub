967f5d89ba5d79598c64534123d26ad3161b741c
Teodora <teodorabancheva@abv.bg>
2019-01-27 17:49:19 +0200

    Add discount entity

---
+++ src/main/java/bg/sofia/fmi/uni/clubhub/entity/DiscountEntity.java
@@ -0,0 +1,53 @@
+package bg.sofia.fmi.uni.clubhub.entity;
+
+import java.util.Date;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "DISCOUNT")
+@Data
+public class DiscountEntity {
+	
+	 @Id
+	 private UUID id;
+	 
+	 @ManyToOne(fetch = FetchType.LAZY, optional = false)
+	 @JoinColumn(name = "customer_id", nullable = false)
+     @OnDelete(action = OnDeleteAction.CASCADE)
+	 private CustomerEntity customer;
+	 
+	 @ManyToOne(fetch = FetchType.LAZY, optional = false)
+	 @JoinColumn(name = "club_id", nullable = false)
+     @OnDelete(action = OnDeleteAction.CASCADE)
+	 private ClubEntity club;
+	 
+	 @Column(name = "start_date", nullable = false)
+	 @Temporal(TemporalType.TIME)
+	 private Date startDate;
+	 
+	 @Column(name = "end_date", nullable = false)
+	 @Temporal(TemporalType.TIME)
+	 private Date endDate;
+	 
+	 @Column(name = "threshold_points", nullable = false)
+	 private int thresholdPoints;
+	 
+	 @Column(name = "percent_off", nullable = false)
+	 private int percentOff;
+	 
+}
