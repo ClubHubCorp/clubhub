03f6ec602f14c7d835c0924f8fa42bea00e1a5da
SimeonGerginov <simeongerginov1@gmail.com>
2019-02-09 15:50:19 +0200

    Added register.html and Navigation
    Signed-off-by: SimeonGerginov <simeongerginov1@gmail.com>

--- src/main/java/bg/sofia/fmi/uni/clubhub/controller/CustomerController.java
+++ src/main/java/bg/sofia/fmi/uni/clubhub/controller/CustomerController.java
@@ -16,6 +16,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,12 +24,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import bg.sofia.fmi.uni.clubhub.model.Customer;
 import bg.sofia.fmi.uni.clubhub.service.ICustomerService;
 
-@RestController
+@Controller
 @RequestMapping("customers")
 public class CustomerController {
 
@@ -37,6 +37,11 @@ public class CustomerController {
     @Autowired
     public CustomerController(ICustomerService customerService) {
         this.customerService = customerService;
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "customers/register";
     }
 
     @GetMapping(value = "{id}", produces = APPLICATION_JSON_VALUE)
