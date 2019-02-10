0c374b81d98d3cb3b865fcb1ae610329e3d686f2
SimeonGerginov <simeongerginov1@gmail.com>
2019-02-10 12:15:26 +0200

    Updated CustomerSecurityConfiguration
    Signed-off-by: SimeonGerginov <simeongerginov1@gmail.com>

--- src/main/java/bg/sofia/fmi/uni/clubhub/security/CustomerSecurityConfiguration.java
+++ src/main/java/bg/sofia/fmi/uni/clubhub/security/CustomerSecurityConfiguration.java
@@ -1,9 +1,9 @@
 package bg.sofia.fmi.uni.clubhub.security;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class CustomerSecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -12,29 +12,37 @@ public class CustomerSecurityConfiguration extends WebSecurityConfigurerAdapter 
     }
 
     protected void configure(HttpSecurity http) throws Exception {
-        http.antMatcher("/customer*")
-                .authorizeRequests()
-                .anyRequest()
-                .hasRole("CUSTOMER")
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/accounts/register").permitAll()
+                .antMatchers("/accounts/login").permitAll();
 
-                .and()
-                .formLogin()
-                .loginPage("/accounts/login-customer")
-                .loginProcessingUrl("/accounts/loginCustomer")
-                .failureUrl("/")
-                .defaultSuccessUrl("/")
+        http.authorizeRequests()
+                .antMatchers("/accounts/register-customer").permitAll()
+                .antMatchers("/accounts/login-customer").permitAll()
+                .antMatchers("/customers/**").hasAuthority("CUSTOMER").anyRequest()
+                .authenticated().and().csrf().disable().formLogin()
+                .loginPage("/accounts/login-customer").failureUrl("/account/login-customer?error=true")
+                .defaultSuccessUrl("/customers")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .and().logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/accounts/logout"))
+                .logoutSuccessUrl("/").and().exceptionHandling()
+                .accessDeniedPage("/access-denied");
 
-                .and()
-                .logout()
-                .logoutUrl("/account/logout")
-                .logoutSuccessUrl("/")
-                .deleteCookies("JSESSIONID")
-
-                .and()
-                .exceptionHandling()
-                .accessDeniedPage("/403")
-
-                .and()
-                .csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/accounts/register-club").permitAll()
+                .antMatchers("/accounts/login-club").permitAll()
+                .antMatchers("/clubs/**").hasAuthority("CLUB").anyRequest()
+                .authenticated().and().csrf().disable().formLogin()
+                .loginPage("/accounts/login-club").failureUrl("/account/login-club?error=true")
+                .defaultSuccessUrl("/clubs")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .and().logout().deleteCookies("JSESSIONID")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/accounts/logout"))
+                .logoutSuccessUrl("/").and().exceptionHandling()
+                .accessDeniedPage("/access-denied");
     }
 }
